c31369f3eb830567156779550e8b085b5a648b88
Seunggyu <seunggyul14@icloud.com>
2023-06-20 15:13:23 -0700

    main page

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,14 +1,16 @@
 import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navRef = useRef()
+    const navigate = useNavigate()
 
     return (
         <header>
            <nav ref={navRef} className='navbar'>
                 <a href='/palette'className='nav_palette'>PALETTE</a>
                 <a href='/book'className='nav_book'>BOOK</a>
-                <img href='/' width={108} height={64} src={'https://cdn.discordapp.com/attachments/1120808152233955398/1120824968368898140/palettebyhazel_logo.png'} alt='LOGO'/>
+                <img  onClick={() => navigate('/')} href='/' className='logo' src={'https://cdn.discordapp.com/attachments/1120808152233955398/1120824968368898140/palettebyhazel_logo.png'} alt='LOGO'/>
                 <a href='/about'className='nav_about'>ABOUT</a>
                 <a href='/contact'className='nav_contact'>CONTACT</a>
             </nav>
