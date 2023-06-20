5f3e33371dda4ab1b35ba710f31dc9746ac201e4
Seunggyu <seunggyul14@icloud.com>
2023-06-20 15:03:39 -0700

    main page

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div className='mainpage'>
-            <img href='/'className='main_pic' src={'https://digitalsynopsis.com/wp-content/uploads/2017/06/beautiful-color-palettes-combinations-schemes-pastel-rainbow.png'} alt='img'/>
+            <img href='/'className='main_pic' src={'https://i.pinimg.com/originals/89/3a/f9/893af9f54eccbde800bf886fe3c0c7c6.jpg'} alt='img'/>
             <p className="homepage_text">PALETTE BY HAZEL IS A BRAND DEDICATED TO PERSONAL COLOR ANALYSIS, DRIVEN BY A MISSION TO CELEBRATE THE UNIQUENESS OF EACH INDIVIDUAL. OUR ULTIMATE AIM IS TO ACHIEVE A HARMONIOUS BALANCE. WITH A PALETTE OF 17 MAIN TYPES, OUR GREATEST SATISFACTION LIES IN WITNESSING INDIVIDUALS DISCOVERING A PROFOUND LOVE AND APPRECIATION FOR THEMSELVES THROUGH THE EXPLORATION OF THEIR OWN IDENTITY.</p>
             <div className="home_info">
                 <ul>
