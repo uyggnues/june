64cf668f2d53fd9251c911b812a40cee7822e629
Seunggyu <seunggyul14@icloud.com>
2023-07-03 20:13:47 -0700

    starting palette

--- src/component/Palette.js
+++ src/component/Palette.js
@@ -4,7 +4,9 @@ const Palette = () => {
     return (
         <div className='every_other_page'>
             <div className='palette_main'>
-            At the heart of our analysis lies the comprehensive color theory, meticulously crafted to identify the most suitable COLORS and TONES based on the inherent traits we possess from birth.
+                <div className='palette_main_text'>
+                    Our color theory is designed to help identify the best colors and tones for individuals based on their inherent traits.
+                </div>
             </div>
         </div>
     );
